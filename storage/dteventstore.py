471e7098bd502b8031e2f52e4feb8de5926985e8
Ghislain Lévêque <ghislain.leveque@gmail.com>
2018-03-03 19:49:51 +0000

    Write implementation

--- storage/dteventstore.py
+++ storage/dteventstore.py
@@ -1,3 +1,5 @@
+import datetime
+import errno
 import os
 
 
@@ -8,19 +10,44 @@ class DatetimeEventStore(object):
     """
 
     def __init__(self, storage_folder=None):
-        self.init_storage(storage_folder)
-
-    def init_storage(self, storage_folder=None):
-        if storage_folder is None:
-            storage_folder = os.path.join(os.path.expanduser("~"), "dts")
-
-        if not os.path.exists(storage_folder):
-            os.makedirs(storage_folder)
-
         self.storage_folder = storage_folder
+        self.init_storage()
+
+    def init_storage(self):
+        if self.storage_folder is None:
+            self.storage_folder = os.path.join(os.path.expanduser("~"), "dts")
+
+        if not os.path.exists(self.storage_folder):
+            os.makedirs(self.storage_folder)
 
     def store_event(self, at=None, data=None):
-        pass
+        if at is None:
+            at = datetime.datetime.now()
+        target = os.path.join(
+            self.storage_folder,
+            at.strftime("%Y"),
+            at.strftime("%m"),
+            at.strftime("%d"),
+            at.strftime("%H:%M:%S"),
+        )
+        try:
+            os.makedirs(os.path.dirname(target))
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+            # Else this just means the folder already exists
+        idx = 1
+        while os.path.exists(
+            "{}-{:0>2d}".format(target, idx)
+        ):
+            idx += 1
+        if idx == 100:
+            raise RuntimeError("Too many events at {}".format(at))
+
+        event_path = "{}-{:0>2d}".format(target, idx)
+        with open(event_path, "w") as fp:
+            fp.write(data)
+        return event_path
 
     def get_events(self, start=None, end=None):
         return []

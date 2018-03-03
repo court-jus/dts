bc73994a66c9c6ebb69e9ba3dea85eafea16faef
Ghislain Lévêque <ghislain.leveque@gmail.com>
2018-03-03 20:09:06 +0000

    Read implementation

--- storage/dteventstore.py
+++ storage/dteventstore.py
@@ -50,4 +50,44 @@ class DatetimeEventStore(object):
         return event_path
 
     def get_events(self, start=None, end=None):
-        return []
+        if start is None:
+            start = datetime.datetime.min
+        if end is None:
+            end = datetime.datetime.now()
+        if start >= end:
+            return []
+        for year in range(start.year, end.year + 1):
+            for event in self.get_year_events(year, start, end):
+                yield event
+
+    def get_year_events(self, year, start, end):
+        start_month = start.month if start.year == year else 1
+        end_month = end.month if end.year == year else 12
+        for month in range(start_month, end_month + 1):
+            month_folder = os.path.join(
+                self.storage_folder,
+                "{:0>4d}".format(year),
+                "{:0>2d}".format(month),
+            )
+            if os.path.exists(month_folder):
+                for name, folders, files in os.walk(month_folder):
+                    for filename in files:
+                        event = self.event_from_file(os.path.join(name, filename))
+                        if event["at"] >= start and event["at"] <= end:
+                            yield event["data"]
+
+    def event_from_file(self, filename):
+        if not os.path.exists(filename):
+            return None
+        if not filename.startswith(self.storage_folder):
+            raise ValueError("This filename is not part of the storage")
+        with open(filename, "r") as fp:
+            data = fp.read()
+        at = filename[len(self.storage_folder):].split("-")[0]
+        if at[0] == "/":
+            at = at[1:]
+        at = datetime.datetime.strptime(at, "%Y/%m/%d/%H:%M:%S")
+        return {
+            "at": at,
+            "data": data,
+        }
